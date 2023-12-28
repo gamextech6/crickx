9291171d384f735b21712014aada2097c87a8aa5
pdeepak936 <67180496+pdeepak936@users.noreply.github.com>
2023-12-28 12:44:27 +0530

    push 1

--- services/push_notification_service.js
+++ services/push_notification_service.js
@@ -6,29 +6,35 @@ const { ONE_SIGNAL_CONFIG } = require("../config/app.config");
 async function SendNotification(data, callback) { 
     var headers = {
         "Content-Type": "application/json; charset=utf-8", 
-        "Authorization": "Basic" + ONE_SIGNAL_CONFIG.API_KEY,
+        Authorization: "Basic " + ONE_SIGNAL_CONFIG.API_KEY,
     };
 
     var options ={
         host: "onesignal.com",
         port: 443, 
-        path: "/api/vi/notifications",
+        path: "/api/v1/notifications", // Corrected API version
         method: "POST",
         headers: headers,
     };
     var req = https.request(options, function (res) {
-        res.on("data", function (data) {
-            console.log(JSON.parse(data));
+        var responseData = '';
 
-            return callback(null, JSON.parse(date));
+        res.on("data", function (chunk) {
+            responseData += chunk;
+        });
+
+        res.on("end", function () {
+            console.log(JSON.parse(responseData));
+            return callback(null, JSON.parse(responseData));
         });
     });
 
     req.on("error", function(e) {
-        return callbak({
+        return callback({
             message: e
         });
     });
+
     req.write(JSON.stringify(data));
     req.end();
 }
